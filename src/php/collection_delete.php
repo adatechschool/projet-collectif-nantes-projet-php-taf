aad58fdcbdc54c2bb5245c073f018ae7d99e987a
flo <boireaufloriane@hotmail.fr>
2025-02-12 11:53:28 +0100

    ajouter, modifier et suprimer une collecte

--- src/php/collection_delete.php
+++ src/php/collection_delete.php
@@ -5,10 +5,7 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $id = (int) $_GET['id'];
 
     try {
-        $pdo = new PDO("mysql:host=localhost;dbname=gestion_collectes", "nom_utilisateur_choisi", "mot_de_passe_solide", [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]);
-
+    
         $stmt = $pdo->prepare("DELETE FROM collectes WHERE id = :id");
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 
