30216cf87dc9f7f31a374349239961cac55944b8
Ange230700 <kouakouangeericstephane@gmail.com>
2025-02-13 16:28:55 +0100

    j'affiche les types de déchets et leurs quantités

--- src/php/collection_list.php
+++ src/php/collection_list.php
@@ -39,7 +39,6 @@ error_reporting(E_ALL);
 <!DOCTYPE html>
 <html lang="fr">
 
-
 <head>
     <!-- <head>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&family=Lora:wght@400;700&family=Montserrat:wght@300;400;700&family=Open+Sans:wght@300;400;700&family=Poppins:wght@300;400;700&family=Playfair+Display:wght@400;700&family=Raleway:wght@300;400;700&family=Nunito:wght@300;400;700&family=Merriweather:wght@300;400;700&family=Oswald:wght@300;400;700&display=swap" rel="stylesheet">
