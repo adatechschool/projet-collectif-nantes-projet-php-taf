f0a444185939b2758ea5d5efc466564097bf433e
Ange230700 <kouakouangeericstephane@gmail.com>
2025-02-13 16:28:55 +0100

    j'affiche les types de déchets et leurs quantités

--- src/php/collection_list.php
+++ src/php/collection_list.php
@@ -12,20 +12,6 @@ try {
                 GROUP BY bc.id_collecte
                 ORDER BY c.date_collecte DESC");
     $collectes = $stmt->fetchAll();
-    
-    $query = $pdo->prepare("SELECT nom FROM benevoles WHERE role = 'admin' LIMIT 1");
-    $query->execute();
-    $admin = $query->fetch(PDO::FETCH_ASSOC);
-    $adminNom = $admin ? htmlspecialchars($admin['nom']) : 'Aucun administrateur trouvé';
-
-    $stmt2 = $pdo->query("
-        SELECT ROUND(SUM(COALESCE(dechets_collectes.quantite_kg,0)),1)
-        AS quantite_total_des_dechets_collectes
-        FROM collectes
-        LEFT JOIN dechets_collectes ON collectes.id=dechets_collectes.id_collecte
-    ");
-
-    $quantite = $stmt2->fetch(PDO::FETCH_ASSOC);
 
     $query = $pdo->prepare("SELECT nom FROM benevoles WHERE role = 'admin' LIMIT 1");
     $query->execute();
