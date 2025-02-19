05d44904ff55604b255b93dfe76e1fcf7f0d50ce
abdallahmassangaritoilibou <abdallah.toilbou@laposte.net>
2025-02-19 14:42:27 +0100

    config

--- src/php/config.php
+++ src/php/config.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 $host = '127.0.0.1';
 $dbname = 'gestion_collectes';
 $username = 'root';
-$password = '';
-
+$password = 'root';
+$port = "8889";
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, [
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false
